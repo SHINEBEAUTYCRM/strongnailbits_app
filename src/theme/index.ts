aee9429d0a4487127a973677d2272e2f57bf3b21
Aleksdandali <gloss.odessa@gmail.com>
2026-02-26 22:27:37 +0200

    fix: remove expo-notifications causing SIGABRT crash on startup

--- src/theme/index.ts
+++
@@ -1,5 +0,0 @@
-export { colors } from './colors';
-export type { ColorKey } from './colors';
-export { fonts, fontSizes, fontWeights, lineHeights } from './typography';
-export { spacing, borderRadius } from './spacing';
-export { shadows } from './shadows';
