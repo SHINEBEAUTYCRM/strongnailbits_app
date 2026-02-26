2da02b3890a10e91f2628ee7b682be16d94fab3a
Aleksdandali <gloss.odessa@gmail.com>
2026-02-27 00:14:17 +0200

    add sentry crash reporting

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,31 @@
 import { Slot } from 'expo-router';
 import { View, Text } from 'react-native';
+import * as Sentry from '@sentry/react-native';
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://979f6a492571297f2efa1e8c5c195177@o4510954476470272.ingest.de.sentry.io/4510954479091792',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
       <Text style={{ fontSize: 24 }}>ShineShop Works!</Text>
       <Slot />
     </View>
   );
-}
+});
